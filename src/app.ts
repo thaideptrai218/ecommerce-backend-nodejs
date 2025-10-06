3edbd7eeee18653a44cef781ce60a9d8ba2ebbd1
tshami218 <welterial@gmail.com>
2025-10-06 10:17:50 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+    const strCompress = "Hello From TipsJs";
+    return res.status(200).json({
+        message: "Welcome Fantipsjs!",
+        metadata: strCompress.repeat(10000),
+    });
+});
+
+export default app;
