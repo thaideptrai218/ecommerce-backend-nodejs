90d821a01021e42df0f2e297bd4988fdd28d922f
tshami218 <welterial@gmail.com>
2025-11-25 00:20:46 +0700

    Handling messages when encountering problems MQ server die

---
+++ src/test/message_queue/rabbitmq/producerDLX.ts
@@ -0,0 +1,39 @@
+import amqplib from "amqplib";
+
+const runProducer = async () => {
+    try {
+        const connection = await amqplib.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+
+        const notificationExchange = "notificationEx";
+        const notificationQueue = "notificationQueueProcess";
+        const notificationExchangeDLX = "notificationExDLX";
+        const notificationRoutingKeyDLX = "notificationRoutingKeyDLX";
+
+        await channel.assertExchange(notificationExchange, "direct", {
+            durable: true,
+        });
+
+        const queueResult = await channel.assertQueue(notificationQueue, {
+            exclusive: false,
+            deadLetterExchange: notificationExchangeDLX,
+            deadLetterRoutingKey: notificationRoutingKeyDLX,
+        });
+
+        await channel.bindQueue(queueResult.queue, notificationExchange, "");
+
+        const msg = "a new product";
+        console.log(`product msg:: `, msg);
+
+        setInterval(() => {
+            channel.sendToQueue(queueResult.queue, Buffer.from(msg), {
+                expiration: 10000,
+            });
+            console.log(`product msg:: `, msg);
+        }, 1000);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+runProducer().catch(console.error);
